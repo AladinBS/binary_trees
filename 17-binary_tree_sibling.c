d5a6502da237d4d03949cf37d1722ff6dbb3379e
AladinBS <aladin.bensassi@gmail.com>
2020-03-11 15:31:22 +0000

    fix

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent == NULL || node == NULL)
 		return (NULL);
 
-	if (node != node->parent->left)
+	if (node->parent->left != node)
 		return (node->parent->left);
 
 	return (node->parent->right);
