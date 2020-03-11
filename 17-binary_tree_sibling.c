86a4c6c64880b833c1847c09d7603b22c02c52af
AladinBS <aladin.bensassi@gmail.com>
2020-03-11 15:28:52 +0000

    fix

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_sibling - sibling of a node
+ * Return: pointer
+ * @node: node
+ *
+ *
+ **/
+
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+
+	if (node->parent == NULL || node == NULL)
+		return (NULL);
+
+	if (node != node->parent->left)
+		return (node->parent->left);
+
+	return (node->parent->right);
+}
