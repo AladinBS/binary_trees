839646143a35cdacc9bd1df4f54a2bcce1386c95
AladinBS <aladin.bensassi@gmail.com>
2020-03-11 14:52:46 +0000

    fix

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,16 @@
 #include "binary_trees.h"
 /**
- * binary_tree_postorder : travel through a binary tree
+ * binary_tree_postorder - going through binary tree
  * Return: nothing
  * @tree: tree
  * @func: pointer
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (func == NULL || tree == NULL)
 		return;
 
-	binary_tree_postorder(tree->right, func);
 	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
 }
