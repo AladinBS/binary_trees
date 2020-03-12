550a0c92d7dc2897edc11cd53d33661dafb73cd6
AladinBS <aladin.bensassi@gmail.com>
2020-03-12 16:01:31 +0000

    second day push

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -16,36 +16,33 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
-
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 
 }
 
 
 /**
  * tree_is_complete - check
- * Return: 1 or 0
+ * @tree: pointer
  * @i: node
  * @cnodes: number
- * @tree: pointer
+ * Return: 1 or 0
  *
  *
- **/
+ */
 
 
 int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
 {
 
-	if (i >= cnodes)
-		return (0);
-
 	if (tree == NULL)
 		return (1);
 
+	if (i >= cnodes)
+		return (0);
 
 	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
 		tree_is_complete(tree->right, (2 * i) + 2, cnodes));
-
 
 }
 
@@ -56,12 +53,11 @@ int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
  * Return: 1 or 0
  *
  *
- **/
+ */
 
 
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-
 
 	size_t cnodes;
 
@@ -71,6 +67,5 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	cnodes = binary_tree_size(tree);
 
 	return (tree_is_complete(tree, 0, cnodes));
-
 
 }
